78378fe34e2f76b4f4c097ca11681334ea1a8bf7
flagsoft <michael.mustun@gmail.com>
2013-10-09 17:24:15 +0200

    Version 1.0.1 (new icon, better user interface, etc)
    Version 1.0.1 (new icon, better user interface, etc)

--- sdspeed/FSAppDelegate.h
+++ sdspeed/FSAppDelegate.h
@@ -2,31 +2,14 @@
 //  FSAppDelegate.h
 //  sdspeed
 //
-//  Created by Michael Mustun on 16.03.13.
+//  Created by M on 16.03.13.
 //  Copyright (c) 2013 Flagsoft. All rights reserved.
 //
 
-/*
- * sdspeed - sdspeed - SD Memeory Card - Fight Flash Fraud
- *
- * Copyright (C) 2013 Michael Mustun
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-
 #import <Cocoa/Cocoa.h>
+
+
+
 
 
 @interface FSAppDelegate : NSObject <NSApplicationDelegate> {
@@ -54,5 +37,11 @@
 @property (weak) IBOutlet NSTextField *UI_readSpeed;
 @property (weak) IBOutlet NSTextField *UI_readUnit;
 
-@property (weak) IBOutlet NSTextField *UI_readProgressPercentage;
+@property (weak) IBOutlet NSTextField *UI_errorMessage;
+
+@property (weak) IBOutlet NSTextField *UI_volumesCapacityGB;
+@property (weak) IBOutlet NSTextField *UI_volumesReadOnly;
+
+- (IBAction)UI_refresh:(id)sender;
+
 @end
--- sdspeed/FSAppDelegate.m
+++ sdspeed/FSAppDelegate.m
@@ -2,29 +2,10 @@
 //  FSAppDelegate.m
 //  sdspeed
 //
-//  Created by Michael Mustun on 16.03.13.
+//  Created by M on 16.03.13.
 //  Copyright (c) 2013 Flagsoft. All rights reserved.
 //
 
-/*
- * sdspeed - sdspeed - SD Memeory Card - Fight Flash Fraud
- *
- * Copyright (C) 2013 Michael Mustun
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
- 
 #import "FSAppDelegate.h"
 
 
@@ -42,25 +23,41 @@
 @synthesize UI_progressValue;
 
 @synthesize UI_volumesName;
+@synthesize UI_volumesCapacityGB;
+@synthesize UI_volumesReadOnly;
 
 @synthesize UI_writeSpeed;
 @synthesize UI_writeUnit;
 
 @synthesize UI_readSpeed;
 @synthesize UI_readUnit;
-@synthesize UI_readProgressPercentage;
+
+@synthesize UI_errorMessage;
 
 
 NSOperationQueue *opQueue=nil;
 
 NSThread *otherThread = nil;
 NSTimer *myTimer = nil;
+NSTimer *myTimer_checkForVolume = nil;
 NSTimer *myTimerStopThread = nil;
-NSString *g_VOLUMES_DRIVE_NAME = nil;
+NSString *g_VOLUMES_DRIVE_NAME = nil;       // "/Volumes/Fotos" UNIX path notation
+NSString *g_VOLUMES_NAME_UI = nil;          // "Fotos" just the volume name
+
+NSNumber *g_VOLUMES_DRIVE_CAPACITY_BYTE = 0;        // volume capacity in bytes
+NSString *g_VOLUMES_DRIVE_CAPACITY_BYTE_UI = nil;   // "--"
+
+double g_VOLUMES_DRIVE_CAPACITY_GB = 0.0;            // 3.680294 Volume capacity in GB (giga byte)
+NSString *g_VOLUMES_DRIVE_CAPACITY_GB_STRING = nil;  // "3.7"
+
+
+NSString *g_VOLUMES_DRIVE_IS_READ_ONLY_UI = nil;
+
+
+NSString *g_ERROR_MESSAGE = nil;            // "No volume inserted. Please insert a volume."
 
 int g_FLAG_shouldTerminate = 0;
-
-int *g_NUMBER_OF_FILES;
+int g_FLAG_volumeThere = 0;         // 0:no volume inserted / 1:volmue inserted
 
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
@@ -68,29 +65,24 @@ int *g_NUMBER_OF_FILES;
     // Insert code here to initialize your application
 
     
+    g_ERROR_MESSAGE = @"";
     
+    [UI_start setEnabled:NO];
 
-    NSArray *keys = [NSArray arrayWithObjects:NSURLVolumeNameKey, NSURLVolumeIsRemovableKey, nil];
-    NSArray *urls = [[NSFileManager defaultManager] mountedVolumeURLsIncludingResourceValuesForKeys:keys options:0];
-        NSString *volumeName;
-    for (NSURL *url in urls) {
-        NSError *error;
-        NSNumber *isRemovable;
 
-        [url getResourceValue:&isRemovable forKey:NSURLVolumeIsRemovableKey error:&error];
-        if ([isRemovable boolValue]) {
-            [url getResourceValue:&volumeName forKey:NSURLVolumeNameKey error:&error];
-            NSLog(@"DEBUG: %@", volumeName);
-        }
-    }
-    
-    
-    g_VOLUMES_DRIVE_NAME = [NSString stringWithFormat:@"/Volumes/%@", volumeName];  //  "/Volumes/NO NAME"
-
-    [UI_volumesName setStringValue:g_VOLUMES_DRIVE_NAME];
-
+    // -- start checking for a volume to be inserted (your SD card)
+    myTimer_checkForVolume = [NSTimer scheduledTimerWithTimeInterval:2.0
+                                               target:self
+                                             selector:@selector(updateUI_checkForVolume:)
+                                             userInfo:nil
+                                              repeats:YES];
 }
 
+- (void)applicationWillTerminate:(NSNotification *)notification
+{
+    [myTimer_checkForVolume invalidate];
+    [myTimer invalidate];
+}
 
 - (void)updateUI:(NSTimer*)timer
 {
@@ -102,15 +94,143 @@ int *g_NUMBER_OF_FILES;
     [UI_progress setDoubleValue:f3write_getCurrentPercent()];
     [UI_progressValue setStringValue:[NSString stringWithFormat:@"%.1f %%", f3write_getCurrentPercent()]];
     
-
-    [UI_readProgressPercentage setDoubleValue:f3read_getCurrentPercent()];
-    NSLog(@"\n---\nDEUBG: f3read_getCurrentPercent=%f", f3read_getCurrentPercent());
     
+}
+
+
+- (void)updateUI_checkForVolume:(NSTimer*)timer
+{
+    
+    NSArray *keys = [NSArray arrayWithObjects:NSURLVolumeNameKey, NSURLVolumeIsRemovableKey, nil];
+    NSArray *urls = [[NSFileManager defaultManager] mountedVolumeURLsIncludingResourceValuesForKeys:keys options:0];
+    NSString *volumeName = nil;
+    NSString *volumeLocalizedName = nil;
+    NSNumber *volumeIsReadOnly = false;
+    NSNumber *volumeCapacity = 0;
+    
+    for (NSURL *url in urls) {
+        NSError *error;
+        NSNumber *isRemovable;
+        
+        [url getResourceValue:&isRemovable forKey:NSURLVolumeIsRemovableKey error:&error];
+        
+        if ([isRemovable boolValue]) {
+            
+            [url getResourceValue:&volumeName forKey:NSURLVolumeNameKey error:&error];
+            [url getResourceValue:&volumeCapacity forKey:NSURLVolumeTotalCapacityKey error:&error];
+            [url getResourceValue:&volumeLocalizedName forKey:NSURLVolumeLocalizedNameKey error:&error];
+
+            [url getResourceValue:&volumeIsReadOnly forKey:NSURLVolumeIsReadOnlyKey error:&error];
+
+            
+            [url getResourceValue:&volumeIsReadOnly forKey:NSURLVolumeIdentifierKey error:&error];
+
+
+
+            g_VOLUMES_DRIVE_CAPACITY_BYTE = volumeCapacity;
+            g_VOLUMES_DRIVE_CAPACITY_BYTE_UI = [NSString stringWithFormat:@"%@", volumeCapacity];
+            g_VOLUMES_DRIVE_CAPACITY_GB = [volumeCapacity doubleValue] / (1024*1024*1024);
+            g_VOLUMES_DRIVE_CAPACITY_GB_STRING = [NSString stringWithFormat:@"%.1f", g_VOLUMES_DRIVE_CAPACITY_GB];
+            
+            g_VOLUMES_DRIVE_IS_READ_ONLY_UI = [NSString stringWithFormat:@"%d", [volumeIsReadOnly intValue] ];
+
+
+            NSLog(@"DEBUG:");
+            NSLog(@" - Name         = %@\n", volumeName);
+            NSLog(@" - Name (local) = %@\n", volumeLocalizedName);
+            NSLog(@" - Capacity = %@ (GB)\n", g_VOLUMES_DRIVE_CAPACITY_GB_STRING);
+            NSLog(@" - Capacity = %@ (Bytes)\n", g_VOLUMES_DRIVE_CAPACITY_BYTE);
+            NSLog(@" - Capacity = %@ (Bytes UI)\n", g_VOLUMES_DRIVE_CAPACITY_BYTE_UI);
+            NSLog(@" - Read only = %@", g_VOLUMES_DRIVE_IS_READ_ONLY_UI);
+
+        }
+    }
+    
+
+    g_VOLUMES_NAME_UI = [NSString stringWithFormat:@"%@", volumeLocalizedName];
+
+    g_VOLUMES_DRIVE_NAME = [NSString stringWithFormat:@"/Volumes/%@", volumeName];
+
+    
+    // TODO: Add a better solution! (Check if a volume is inserted or not.)
+    //
+    // possible names (NSString) ar:
+    //   "/Volume/Fotos"   --> The volume name is Fotos
+    //   "/Volume/NO NAME" --> The volume name is NO NAME
+    //   "/Volume/(null)"  --> We assume here that there is no volume inserted.
+    //
+    // Note: For the following, we assume here that a drive name "(null)" as string means no volume inserted, don't know what happen here if the volume name is acutally "(null)".
+
+    if ( [g_VOLUMES_NAME_UI isEqualToString:@"(null)"] ) {
+
+        //
+        // -- no SD card volume there
+        //
+
+        g_FLAG_volumeThere = 0;
+        g_ERROR_MESSAGE = @"Please insert a SD card.";
+        
+        g_VOLUMES_DRIVE_NAME = @"-";
+        g_VOLUMES_NAME_UI = @"-";
+        g_VOLUMES_DRIVE_CAPACITY_GB_STRING = @"-";
+        g_VOLUMES_DRIVE_CAPACITY_BYTE = 0;
+        g_VOLUMES_DRIVE_CAPACITY_BYTE_UI = @"-";
+        
+        g_VOLUMES_DRIVE_IS_READ_ONLY_UI = @"-";
+
+        
+        [UI_volumesCapacityGB setStringValue:@"-"];
+        
+
+        [UI_volumesReadOnly setStringValue:@"-"];
+
+        [UI_start setEnabled:NO];
+
+        
+    } else {
+        
+        //
+        // -- SD card volume there
+        //
+        
+        g_FLAG_volumeThere = 1;
+
+        
+        [UI_volumesCapacityGB setStringValue:[NSString stringWithFormat:@"%@ GB (%@ Bytes)", g_VOLUMES_DRIVE_CAPACITY_GB_STRING, g_VOLUMES_DRIVE_CAPACITY_BYTE_UI]];
+        
+        if ( [g_VOLUMES_DRIVE_IS_READ_ONLY_UI isEqualToString:@"1"] ) {
+            // read only media
+            [UI_volumesReadOnly setStringValue:@"read only"];
+            g_ERROR_MESSAGE = @"Please make card writeable.";
+            [UI_start setEnabled:NO];
+        } else {
+            [UI_volumesReadOnly setStringValue:@"read write"];
+            [UI_start setEnabled:YES];
+        }
+        
+   
+        
+    }
+
+    // -- UI update
+    [UI_volumesName setStringValue:g_VOLUMES_NAME_UI];
+    [UI_errorMessage setStringValue:g_ERROR_MESSAGE];
 }
 
 - (IBAction)UI_start_pressed:(id)sender {
 
     //[UI_start state] == NSOnState
+
+    if (!g_FLAG_volumeThere) {
+        // -- no volume there so display a message and exit
+        [UI_errorMessage setStringValue:@"Please insert a SD card first and make sure it is not locked."];
+        return;
+    }
+    
+    
+    
+    // -- stop checking for volumes
+    [myTimer_checkForVolume invalidate];
 
     
     [UI_start setTitle:@"Please wait..."];
@@ -198,16 +318,12 @@ int *g_NUMBER_OF_FILES;
     NSLog(@"AV SPEED: %f", speed_write);
 
     //
-    // -- all done
+    // -- write test done
     //
     
-    [myTimer invalidate];
-        
+    
     [UI_writeSpeed setStringValue:[NSString stringWithFormat:@"%.2f", speed_write]];
     
-    [UI_start setEnabled:YES];
-    [UI_start setTitle:@"Start"];
-
 
     [UI_progress setDoubleValue:100.0];
     [UI_progressValue setStringValue:[NSString stringWithFormat:@"%.1f %%", 100.0]];
@@ -222,9 +338,12 @@ int *g_NUMBER_OF_FILES;
     // -- read test
     //
 
+    
+    [UI_start setTitle:@"Do read test please wait..."];
+    
     int read_start_at;
 	const char *read_path;
-	int *read_files;
+	const int *read_files;
 	int read_progress;
     
     
@@ -232,16 +351,22 @@ int *g_NUMBER_OF_FILES;
     read_path = [g_VOLUMES_DRIVE_NAME UTF8String];
     
     read_files = ls_my_files(read_path, read_start_at);
-    
-    g_NUMBER_OF_FILES = read_files;
-    
-    printf("DEBUG: g_NUMBER_OF_FILES=%d\n", *g_NUMBER_OF_FILES);
-    
 	//If stdout isn't a terminal, supress progress.
-	read_progress = isatty(STDOUT_FILENO);
+	//read_progress = isatty(STDOUT_FILENO);
     read_progress = 1;
 	f3read_iterate_files(read_path, read_files, read_start_at, read_progress);
 	free((void *)read_files);
+
+
+
+    //
+    // -- all done (write and read test)
+    //
+    
+    [myTimer invalidate];
+
+    [UI_start setEnabled:YES];
+    [UI_start setTitle:@"Start"];
 
     
     [UI_readSpeed setStringValue:[NSString stringWithFormat:@"%.2f", f3read_getReadSpeedAverage()]];
@@ -250,8 +375,24 @@ int *g_NUMBER_OF_FILES;
     
     // -- clean up files
     f3write_unlink_old_files([g_VOLUMES_DRIVE_NAME UTF8String], 0);
+
+    
+    
+    // -- start checking for volume again
+    myTimer_checkForVolume = [NSTimer scheduledTimerWithTimeInterval:2.0
+                                                              target:self
+                                                            selector:@selector(updateUI_checkForVolume:)
+                                                            userInfo:nil
+                                                             repeats:YES];
+
+    
     
 }
 
 
+- (IBAction)UI_refresh:(id)sender {
+    
+    [self updateUI_checkForVolume:myTimer_checkForVolume];
+    
+}
 @end
